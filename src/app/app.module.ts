d79dac99c40041a42534a440f114077359c78440
Jose Daniel Vasquez Garcia <josedaniel.vasquez@globant.com>
2019-02-07 15:47:51 -0500

    some issues fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppComponent } from './app.component';
 
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,16 +3,23 @@ import { CommonModule } from '@angular/common';
 
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatChipsModule
+    MatChipsModule,
+    MatProgressBarModule, 
+    MatButtonModule
   ],
   exports: [
     MatToolbarModule,
-    MatChipsModule
+    MatChipsModule,
+    MatProgressBarModule,
+    MatButtonModule
   ],
   declarations: []
 })
